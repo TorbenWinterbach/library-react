5441a2c31d45b90bc933a8a6187e12ec903a1953
TorbenWinterbach <torbenwinterbach2003@gmail.com>
2022-10-04 20:51:50 +0200

    Init commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import Footer from "./components/Footer.jsx";
+import Nav from "./components/Nav.jsx";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import Books from "./pages/Books.jsx";
+import { books } from "./data.js";
+import BookInfo from "./pages/BookInfo.jsx";
+import Cart from "./pages/Cart.jsx";
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [cart, setCart] = useState([]);
+
+  function addToCart(book) {
+    setCart([...cart, { ...book, quantity: 1 }]);
+  }
+
+  function removeItem(item) {
+    setCart(cart.filter(book => book.id !== item.id))
+    console.log('remove item', item)
+  }
+
+  function changeQuantity(book, quantity) {
+    setCart(
+      cart.map((item) => 
+        item.id === book.id
+          ? {
+              ...item,
+              quantity: +quantity,
+            }
+          : item
+      )
+    );
+  }
+
+  function numberOfItems() {
+    let counter = 0;
+    cart.forEach(item => {
+      counter += item.quantity
+    })
+    return counter
+  }
+
+  useEffect(() => {
+    console.log(cart);
+  }, [cart]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Nav numberOfItems={numberOfItems()} />
+        <Route path="/" exact render={() => <Home books={books} />} />
+        <Route path="/books" exact render={() => <Books books={books} />} />
+        <Route
+          path="/books/:id"
+          exact
+          render={() => (
+            <BookInfo books={books} cart={cart} addToCart={addToCart} />
+          )}
+        />
+        <Route
+          path="/cart"
+          render={() => (
+            <Cart cart={cart} books={books} removeItem={removeItem} changeQuantity={changeQuantity} />
+          )}
+        />
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Landing.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import UndrawBooks from '../assets/Undraw_Books.svg'
+
+const Landing = () => {
+    return (
+        <div>
+            <section id='Landing'>
+                <header>
+                    <div className="header__container">
+                        <div className="header__description">
+                            <h1>UK's most beloved online library platform</h1>
+                            <h2>Find your dream book with <span className='purple'>Library</span></h2>
+                            <Link to="#features">
+                                <button className='btn'>Browse books</button>
+                            </Link>
+                        </div>
+                        <figure className='header__img--wrapper'>
+                            <img src={UndrawBooks} alt="" />
+                        </figure>
+                    </div>
+                </header>
+            </section>
+        </div>
+    )
+}
+
+export default Landing
