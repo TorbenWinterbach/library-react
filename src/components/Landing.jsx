40050eaff01fee8dd071bc3c78a6218818b25637
TorbenWinterbach <torbenwinterbach2003@gmail.com>
2022-10-04 22:56:22 +0200

    init commit

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -11,7 +11,7 @@ const Landing = () => {
                         <div className="header__description">
                             <h1>UK's most beloved online library platform</h1>
                             <h2>Find your dream book with <span className='purple'>Library</span></h2>
-                            <Link to="#features">
+                            <Link to="/books">
                                 <button className='btn'>Browse books</button>
                             </Link>
                         </div>
